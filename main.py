786ae3b516fdcdc9272ac0a498e440ed204fb9cf
Thomas Volant <duc@azsystem.fr>
2025-05-01 12:47:27 +0200

    add game to list + add reading of list, import into var then close the reading

--- main.py
+++ main.py
@@ -3,6 +3,25 @@
 # https://docs.python.org/3.6/library/argparse.html#required
 import argparse
 parser = argparse.ArgumentParser(description='Give me a white russian')
-parser.add_argument('-u', metavar='username', type=str, help='Name of the user passed as Arg.', required=True)
+parser.add_argument('--list-path', metavar='Games List Path', type=str, help='Path containing the List of Games', dest="listPath", required=True)
 args = parser.parse_args()
-print (f"Hello World! {args.u}")
+# open resout les chemins relatifs en fonction du PWD
+targetGamesFileDescriptor= open(args.listPath, 'r')
+content = targetGamesFileDescriptor.read()
+print (content)
+targetGamesFileDescriptor.close()
+print (content)
+# path-exist = cat {args.listPath}
+# if
+#   path-exist = True
+#   print (f"The file is located in {args.listPath}")
+# else
+#   path-exist = error
+#   exit 1
+# pas de "with"
+
+# Cat le path, si pas error alors print si error exit 1
+
+# Ouvrir le fichier pour obtenir une str contenant le contenu du fichier
+# Parser le fichier de json vers un dict python
+# Parcourir le dict python et afficher les valeurs 
