03a5733edb0b0d7e654d31cf664b1d9e4d8e580c
Thomas Volant <duc@azsystem.fr>
2025-07-18 15:34:00 +0200

    add function to lis games

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ import json
 parser = argparse.ArgumentParser(description='Give me a white russian')
 parser.add_argument('--list-path', metavar='Games List Path', type=str, help='Path containing the List of Games', dest="listPath", required=True)
 args = parser.parse_args()
-# open resout les chemins relatifs en fonction du PWD
-targetGamesFileDescriptor= open(args.listPath, 'r')
-content = targetGamesFileDescriptor.read()
-print (content)
-targetGamesFileDescriptor.close()
-loadedcontent = json.loads(content)
-#print (loadedcontent["games"][1]["title"])
+
 def list_games(games):
     for item in games:
         result = f"I own {item['title']} released in {item['release_year']} for {", ".join(item['platforms'])}"
         print (result)
-  
-list_games(loadedcontent["games"])
+
+def extract_games(pathlist):
+    targetGamesFileDescriptor= open(pathlist, 'r')
+    content = targetGamesFileDescriptor.read()
+    targetGamesFileDescriptor.close()
+    loadedcontent = json.loads(content)
+    return loadedcontent["games"]
+
+games = extract_games(args.listPath)
+list_games(games)
 # path-exist = cat {args.listPath}
 # if
 #   path-exist = True
@@ -29,7 +31,3 @@ list_games(loadedcontent["games"])
 # pas de "with"
 
 # Cat le path, si pas error alors print si error exit 1
-
-# Ouvrir le fichier pour obtenir une str contenant le contenu du fichier
-# Parser le fichier de json vers un dict python
-# Parcourir le dict python et afficher les valeurs 
